7cf2a99dd62fe95c622699330f4dcb4bb84fedee
vedantshetti <vedantshetti123456@gmail.com>
2025-04-08 17:42:57 +0530

    added the file structure setup in the code

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Home from "./components/Home";
+import Jobs from "./components/Jobs";
+import Browse from "./components/Browse";
+import Profile from "./components/Profile";
+import JobDescription from "./components/JobDescription";
+import Companies from "./components/admin/Companies";
+import CompanyCreate from "./components/admin/CompanyCreate";
+import CompanySetup from "./components/admin/CompanySetup";
+
+const appRouter = createBrowserRouter([
+  // user side routes 
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/jobs",
+    element: <Jobs />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/browse",
+    element: <Browse/>,
+  },
+  {
+    path: "/profile",
+    element: <Profile/>,
+  },
+
+  {
+    path: "/description/:id",
+    element: <JobDescription/>,
+  },
+
+  // recuieter side routes 
+
+  {
+    path:'/admin/companies',
+    element:<Companies/>,
+  },
+  {
+    path:'/admin/companies/create',
+    element:<CompanyCreate/>,
+  },
+  {
+    path:'/admin/companies/:id',
+    element:<CompanySetup/>,
+  }
+
+
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={appRouter} />
+    </>
+  );
+}
+
+export default App;
